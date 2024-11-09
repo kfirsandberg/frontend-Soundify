53c8edcdf085f333edb56c5c787508c8fb68c240
avivmor97 <aviv.mor97@gmail.com>
2024-11-09 15:28:52 +0200

    adjusted main layout

--- src/RootCmp.jsx
+++ src/RootCmp.jsx
@@ -1,29 +1,29 @@
 import React from 'react'
-import { Routes, Route } from 'react-router'
+import { Routes, Route } from 'react-router-dom'
 
 import { StationIndex } from './pages/StationIndex.jsx'
-
 import { AppHeader } from './cmps/AppHeader'
 import { AppPlayer } from './cmps/AppPlayer.jsx'
-// import { UserMsg } from './cmps/UserMsg.jsx'
 import { StationDetails } from './pages/StationDetails.jsx'
 import { Library } from "./cmps/Library.jsx"
+
 export function RootCmp() {
     return (
         <div className="main-container">
-            <AppHeader />
-            {/* <UserMsg /> */}
-            <main>
-                <h2>aviv kfir amit</h2>
-                    <StationIndex />
-                {/* <Routes>
-                    <Route path="playlist/:playlistId" element={<StationDetails />} />
-                </Routes> */}
-            </main>
-            <Library />
-            <AppPlayer />
+            <AppHeader className="header" />
+            <div className="content">
+                <aside className="library-route">
+                    <Library />
+                </aside>
+                <main className="station-index-route">
+                    <h2>aviv kfir amit</h2>
+                    <Routes>
+                        <Route path="/" element={<StationIndex />} />
+                        <Route path="playlist/:playlistId" element={<StationDetails />} />
+                    </Routes>
+                </main>
+            </div>
+            <AppPlayer className="player" />
         </div>
     )
 }
-
-
