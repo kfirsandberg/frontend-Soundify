57edf614ce1c4edca4b7584f1d8acd205543f63d
avivmor97 <aviv.mor97@gmail.com>
2024-11-09 15:38:46 +0200

    basic lib set up

--- src/cmps/LibraryList.jsx
+++ src/cmps/LibraryList.jsx
@@ -1,12 +1,38 @@
+import { useEffect, useState } from "react"
+import { stationLocalService } from "../services/station/station.service.local"
 
 export function LibraryList() {
-	return (
-		<header className="library-ist">
+    const [stations, setStations] = useState(null)  // Initializing stations state
+
+    useEffect(() => {
+        loadStations()
+    }, [])  // Run this effect only once when component mounts
+
+    function loadStations() {
+        stationLocalService.query()
+            .then(data => {
+                setStations(data)  // Set stations data from local service
+            })
+            .catch(error => {
+                console.error('Error fetching stations:', error)
+            })
+    }
+
+    // Loading state to display while data is being fetched
+    if (stations === null) {
+        return <div>Loading...</div>
+    }
+
+    return (
+        <div className="library-list">
             <ul>
-                <li>liked songs</li>
-                <li>techno</li>
-                <li>ישראלי</li>
+                {stations.map(station => (
+                    <li key={station._id}>
+                        <h3>{station.name}</h3>
+                        <img src={station.imgURL} alt={station.name} />
+                    </li>
+                ))}
             </ul>
-		</header>
-	)
+        </div>
+    )
 }
