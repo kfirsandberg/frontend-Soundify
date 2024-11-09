eec537199264200a00ff198dd483556470e25e93
kfirsandberg <kfirsan07@gmail.com>
2024-11-09 21:20:33 +0200

    adding style to all of the cmps

--- src/cmps/StationHeader.jsx
+++ src/cmps/StationHeader.jsx
@@ -1,15 +1,18 @@
 
-export function StationHeader({station}) {
+export function StationHeader({ station }) {
     console.log(station);
-    
+
     return (
         <section className="station-details">
-            <p>playlist</p>
             <img src={station.imgURL} alt="" />
-            <h2>{station.name}</h2>
-            <p>playlist description</p>
-            <button>user name</button>
-            <p>playlist info</p>
+            <section className="playlist-details">
+                <p>playlist</p>
+                <h2>{station.name}</h2>
+                <p>playlist description</p>
+                <button>user name</button>
+                <p>playlist info</p>
+
+            </section>
         </section>
     )
 }
--- src/cmps/StationPreview.jsx
+++ src/cmps/StationPreview.jsx
@@ -1,16 +1,22 @@
 import playIcon from '../../public/assets/play.svg';
+import { loadSong } from '../store/actions/station.actions.js'
 
 export function StationPreview({ station }) {
-    const { name, imgURL } = station;
-  
-    return (
-      <section className="station-preview">
-        <img src={imgURL} alt={name} />
-        <div className="play-button-container">
-          <button className="play-button"><img src={playIcon} alt="" /></button>
-        </div>
-        <h3>{name}</h3>
-      </section>
-    );
+  const { name, imgURL } = station;
+
+  function onPlay(ev) {
+    ev.stopPropagation()
+
+    loadSong(station.songs[0].id)
+
   }
-  
+  return (
+    <section className="station-preview">
+      <img src={imgURL} alt={name} />
+      <div className="play-button-container">
+        <button className="play-button"><img className='play' src={playIcon} onClick={onPlay} /></button>
+      </div>
+      <h3>{name}</h3>
+    </section>
+  );
+}
