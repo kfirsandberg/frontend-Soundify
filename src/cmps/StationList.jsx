1e93aeabf1cd8385576bae166a423f2bb94cc070
avivmor97 <aviv.mor97@gmail.com>
2024-11-12 10:35:10 +0200

    adjusted station list

--- src/cmps/StationList.jsx
+++ src/cmps/StationList.jsx
@@ -2,6 +2,7 @@ import { StationPreview } from "../cmps/StationPreview.jsx"
 import { useNavigate } from 'react-router-dom';
 
 import { loadStation } from "../store/actions/station.actions.js";
+
 export function StationList({ stations }) {
     const navigate = useNavigate()
 
@@ -13,12 +14,11 @@ export function StationList({ stations }) {
     return (
         <section className="station-list-container">
             <header className="station-list-header-container">
-                <h2 className="station-list-header" >Created for you</h2>
-
-                <p className="station-list-header-2">Show all </p>
+                <h2 className="station-list-header">Created for you</h2>
+                <p className="station-list-header-2">Show all</p>
             </header>
             <section className="station-list">
-                {stations.map(station => (
+                {stations.slice(0,7).map(station => (
                     <button className="station-preview"
                         key={station._id}
                         onClick={() => onClickStation(station)}
@@ -26,9 +26,7 @@ export function StationList({ stations }) {
                         <StationPreview station={station} />
                     </button>
                 ))}
-
             </section>
         </section>
-
     )
 }
