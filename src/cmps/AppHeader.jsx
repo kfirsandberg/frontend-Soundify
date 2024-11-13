1ca92d614c4536c93a7e37eb3fcd8c7375ca1c45
Amit Baruch <amitbaruch08@gmail.com>
2024-11-13 09:33:32 +0200

    Update AppHeader to dynamically display home icon based on current path

--- src/cmps/AppHeader.jsx
+++ src/cmps/AppHeader.jsx
@@ -1,9 +1,13 @@
 import React, { useState, useRef } from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useParams, useLocation } from 'react-router-dom'
 
 export function AppHeader() {
     const [focused, setFocused] = useState(false)
     const inputWrapperRef = useRef(null)
+    const params = useParams()
+    const location = useLocation()
+
+    const isHomePage = location.pathname === '/'
 
     function handleFocus() {
         setFocused(true)
@@ -24,19 +28,35 @@ export function AppHeader() {
             <div className="header-center-content">
                 <Link to="/">
                     <button className="header-home-btn" title="Home">
-                        <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            data-encore-id="icon"
-                            role="img"
-                            aria-hidden="true"
-                            viewBox="0 0 24 24"
-                            className="home-icon Svg-sc-ytk21e-0 bneLcE"
-                        >
-                            <path
-                                d="M12.5 3.247a1 1 0 0 0-1 0L4 7.577V20h4.5v-6a1 1 0 0 1 1-1h5a1 1 0 0 1 1 1v6H20V7.577l-7.5-4.33zm-2-1.732a3 3 0 0 1 3 0l7.5 4.33a2 2 0 0 1 1 1.732V21a1 1 0 0 1-1 1h-6.5a1 1 0 0 1-1-1v-6h-3v6a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V7.577a2 2 0 0 1 1-1.732l7.5-4.33z"
-                                fill="#b3b3b3"
-                            />
-                        </svg>
+                        {isHomePage ? (
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                data-encore-id="icon"
+                                role="img"
+                                aria-hidden="true"
+                                viewBox="0 0 24 24"
+                                className="home-icon Svg-sc-ytk21e-0 bneLcE"
+                            >
+                                <path
+                                    d="M13.5 1.515a3 3 0 0 0-3 0L3 5.845a2 2 0 0 0-1 1.732V21a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1v-6h4v6a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V7.577a2 2 0 0 0-1-1.732l-7.5-4.33z"
+                                    fill="#ffffff"
+                                />
+                            </svg>
+                        ) : (
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                data-encore-id="icon"
+                                role="img"
+                                aria-hidden="true"
+                                viewBox="0 0 24 24"
+                                className="home-icon Svg-sc-ytk21e-0 bneLcE"
+                            >
+                                <path
+                                    d="M12.5 3.247a1 1 0 0 0-1 0L4 7.577V20h4.5v-6a1 1 0 0 1 1-1h5a1 1 0 0 1 1 1v6H20V7.577l-7.5-4.33zm-2-1.732a3 3 0 0 1 3 0l7.5 4.33a2 2 0 0 1 1 1.732V21a1 1 0 0 1-1 1h-6.5a1 1 0 0 1-1-1v-6h-3v6a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V7.577a2 2 0 0 1 1-1.732l7.5-4.33z"
+                                    fill="#b3b3b3"
+                                />
+                            </svg>
+                        )}
                     </button>
                 </Link>
 
