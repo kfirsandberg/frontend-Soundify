96ff6ee1e371a48e906467d3831392d9a1a7ac8a
Amit Baruch <amitbaruch08@gmail.com>
2024-11-13 08:48:16 +0200

    Touch ups

--- src/RootCmp.jsx
+++ src/RootCmp.jsx
@@ -1,4 +1,4 @@
-import React, { useState ,useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
 import { Routes, Route } from 'react-router-dom'
 import { useSelector } from 'react-redux'
 import { AllStations } from './pages/AllStations.jsx'
@@ -9,31 +9,31 @@ import { StationDetails } from './pages/StationDetails.jsx'
 import { Library } from './cmps/Library.jsx'
 import { YouTubeAudioPlayer } from './cmps/YouTubeAudioPlayer.jsx'
 import { setBgColor } from './store/actions/station.actions.js'
-import { FastAverageColor } from 'fast-average-color';
+import { FastAverageColor } from 'fast-average-color'
 
-const fac = new FastAverageColor();
+const fac = new FastAverageColor()
 export function RootCmp() {
     const [isLibraryActive, setIsLibraryActive] = useState(false) // Track the active state of the library
 
     // Toggle the library route active state
     const toggleLibraryActive = () => setIsLibraryActive(prev => !prev)
-    const stations = useSelector(storeState => storeState.stationModule.stations);
-    const bgColor = useSelector(storeState => storeState.stationModule.bgColor);
+    const stations = useSelector(storeState => storeState.stationModule.stations)
+    const bgColor = useSelector(storeState => storeState.stationModule.bgColor)
 
     useEffect(() => {
         const fetchBackgroundColor = async () => {
             if (stations && stations.length > 0 && stations[0].imgURL) {
                 try {
-                    const color = await fac.getColorAsync(stations[0].imgURL);
+                    const color = await fac.getColorAsync(stations[0].imgURL)
                     setBgColor(color.rgb)
                 } catch (error) {
-                    console.error('Error fetching average color:', error);
+                    console.error('Error fetching average color:', error)
                 }
             }
-        };
-        
-        fetchBackgroundColor();
-    }, [stations]);
+        }
+
+        fetchBackgroundColor()
+    }, [stations])
 
     return (
         <div className="main-container">
@@ -44,8 +44,10 @@ export function RootCmp() {
                     <Library toggleLibraryActive={toggleLibraryActive} /> {/* Pass toggle function to Library */}
                 </aside>
 
-                <main className="station-index-route"
-                 style={{ background: `linear-gradient(to top, #121212 70%,  ${bgColor}  100%)` }}>
+                <main
+                    className="station-index-route"
+                    style={{ background: `linear-gradient(to top, #121212 70%,  ${bgColor}  100%)` }}
+                >
                     <Routes>
                         <Route path="/" element={<StationIndex />} />
                         <Route path="/all-stations" element={<AllStations />} /> {/* Add AllStations route */}
