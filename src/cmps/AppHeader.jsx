60c3076f3f72ec0af645cce5c463f622f5eb8727
Amit Baruch <amitbaruch08@gmail.com>
2024-11-16 17:13:07 +0200

    Update browse icon

--- src/cmps/AppHeader.jsx
+++ src/cmps/AppHeader.jsx
@@ -82,16 +82,39 @@ export function AppHeader() {
                     />
                     <Link to="/browse" style={{ zIndex: 1000 }}>
                         <button className="header-browse-btn" title="Browse" onClick={() => navigate('/browse')}>
-                            <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                data-encore-id="icon"
-                                role="img"
-                                aria-hidden="true"
-                                viewBox="0 0 24 24"
-                                className="browse-icon Svg-sc-ytk21e-0 bneLcE"
-                            >
-                                <path d="M4 2a1 1 0 0 1 1-1h14a1 1 0 0 1 1 1v4H4V2zM1.513 9.37A1 1 0 0 1 2.291 9H21.71a1 1 0 0 1 .978 1.208l-2.17 10.208A2 2 0 0 1 18.562 22H5.438a2 2 0 0 1-1.956-1.584l-2.17-10.208a1 1 0 0 1 .201-.837zM12 17.834c1.933 0 3.5-1.044 3.5-2.333 0-1.289-1.567-2.333-3.5-2.333S8.5 14.21 8.5 15.5c0 1.289 1.567 2.333 3.5 2.333z" />
-                            </svg>
+                            {location.pathname === '/browse' ? (
+                                <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    data-encore-id="icon"
+                                    role="img"
+                                    aria-hidden="true"
+                                    viewBox="0 0 24 24"
+                                    className="browse-icon Svg-sc-ytk21e-0 bneLcE"
+                                >
+                                    <path
+                                        d="M4 2a1 1 0 0 1 1-1h14a1 1 0 0 1 1 1v4H4V2zM1.513 9.37A1 1 0 0 1 2.291 9H21.71a1 1 0 0 1 .978 1.208l-2.17 10.208A2 2 0 0 1 18.562 22H5.438a2 2 0 0 1-1.956-1.584l-2.17-10.208a1 1 0 0 1 .201-.837zM12 17.834c1.933 0 3.5-1.044 3.5-2.333 0-1.289-1.567-2.333-3.5-2.333S8.5 14.21 8.5 15.5c0 1.289 1.567 2.333 3.5 2.333z"
+                                        fill="#ffffff"
+                                    />
+                                </svg>
+                            ) : (
+                                <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    data-encore-id="icon"
+                                    role="img"
+                                    aria-hidden="true"
+                                    viewBox="0 0 24 24"
+                                    className="browse-icon-empty Svg-sc-ytk21e-0 bneLcE"
+                                >
+                                    <path
+                                        d="M15 15.5c0 1.105-1.343 2-3 2s-3-.895-3-2 1.343-2 3-2 3 .895 3 2z"
+                                        fill="#b3b3b3"
+                                    />
+                                    <path
+                                        d="M1.513 9.37A1 1 0 0 1 2.291 9h19.418a1 1 0 0 1 .979 1.208l-2.339 11a1 1 0 0 1-.978.792H4.63a1 1 0 0 1-.978-.792l-2.339-11a1 1 0 0 1 .201-.837zM3.525 11l1.913 9h13.123l1.913-9H3.525zM4 2a1 1 0 0 1 1-1h14a1 1 0 0 1 1 1v4h-2V3H6v3H4V2z"
+                                        fill="#b3b3b3"
+                                    />
+                                </svg>
+                            )}
                         </button>
                     </Link>
                 </div>
