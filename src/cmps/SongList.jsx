219a3858a24151bc11ddd662137dffdf050bd54e
amitbaruchh <amitbaruch08@gmail.com>
2024-11-17 12:18:53 +0200

    Style Like Songs button positioning in Song List

--- src/cmps/SongList.jsx
+++ src/cmps/SongList.jsx
@@ -285,10 +285,11 @@ export function SongList() {
                                                         </Box>
                                                     </Box>
 
-                                                    {/* Album and Duration columns */}
+                                                    {/* Album, Like Button, and Duration columns */}
                                                     <Box
                                                         sx={{
                                                             display: 'flex',
+                                                            alignItems: 'center',
                                                             justifyContent: 'space-between',
                                                             marginLeft: 'auto',
                                                             width: '50%',
@@ -300,7 +301,6 @@ export function SongList() {
                                                                 textAlign: 'left',
                                                                 color: 'rgba(255, 255, 255, 0.6)',
                                                                 marginRight: 10,
-                                                                marginLeft: -3,
                                                                 cursor: 'pointer',
                                                                 '&:hover': {
                                                                     textDecoration: 'underline',
@@ -310,32 +310,35 @@ export function SongList() {
                                                         >
                                                             {song.album}
                                                         </Typography>
-                                                        <button onClick={() => toggleLike(song)} title={song.liked ? "Remove from liked songs" : "Add to liked songs"}
-                                                            className="liked-songs-btn">
-                                                            {song.liked ? (
-                                                                <svg xmlns="http://www.w3.org/2000/svg" role="img" aria-hidden="true" viewBox="0 0 16 16"
-                                                                    className='liked-icon'
-                                                                >
-                                                                    <path d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm11.748-1.97a.75.75 0 0 0-1.06-1.06l-4.47 4.47-1.405-1.406a.75.75 0 1 0-1.061 1.06l2.466 2.467 5.53-5.53z" />
-                                                                </svg>
-                                                            ) : (
-                                                                <svg xmlns="http://www.w3.org/2000/svg" role="img" aria-hidden="true" viewBox="0 0 24 24"
-                                                                    className='not-liked-icon'
-                                                                >
-                                                                    <path d="M11.999 3a9 9 0 1 0 0 18 9 9 0 0 0 0-18zm-11 9c0-6.075 4.925-11 11-11s11 4.925 11 11-4.925 11-11 11-11-4.925-11-11z" />
-                                                                    <path d="M17.999 12a1 1 0 0 1-1 1h-4v4a1 1 0 1 1-2 0v-4h-4a1 1 0 1 1 0-2h4V7a1 1 0 1 1 2 0v4h4a1 1 0 0 1 1 1z" />
-                                                                </svg>
-                                                            )}
-                                                        </button>
-
-
-                                                        <Typography
-                                                            variant="body2"
-                                                            sx={{ color: 'rgba(255, 255, 255, 0.6)', marginLeft: -3 }}
+                                                        <Box
+                                                            sx={{
+                                                                display: 'flex',
+                                                                alignItems: 'center',
+                                                                gap: '10px', 
+                                                            }}
                                                         >
-                                                            {song.duration}
-                                                        </Typography>
+                                                            <button
+                                                                onClick={() => toggleLike(song)}
+                                                                title={song.liked ? 'Remove from liked songs' : 'Add to liked songs'}
+                                                                className="liked-songs-btn"
+                                                            >
+                                                                {song.liked ? (
+                                                                    <svg xmlns="http://www.w3.org/2000/svg" role="img" aria-hidden="true" viewBox="0 0 16 16" className="liked-icon">
+                                                                        <path d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm11.748-1.97a.75.75 0 0 0-1.06-1.06l-4.47 4.47-1.405-1.406a.75.75 0 1 0-1.061 1.06l2.466 2.467 5.53-5.53z" />
+                                                                    </svg>
+                                                                ) : (
+                                                                    <svg xmlns="http://www.w3.org/2000/svg" role="img" aria-hidden="true" viewBox="0 0 24 24" className="not-liked-icon">
+                                                                        <path d="M11.999 3a9 9 0 1 0 0 18 9 9 0 0 0 0-18zm-11 9c0-6.075 4.925-11 11-11s11 4.925 11 11-4.925 11-11 11-11-4.925-11-11z" />
+                                                                        <path d="M17.999 12a1 1 0 0 1-1 1h-4v4a1 1 0 1 1-2 0v-4h-4a1 1 0 1 1 0-2h4V7a1 1 0 1 1 2 0v4h4a1 1 0 0 1 1 1z" />
+                                                                    </svg>
+                                                                )}
+                                                            </button>
+                                                            <Typography variant="body2" sx={{ color: 'rgba(255, 255, 255, 0.6)' }}>
+                                                                {song.duration}
+                                                            </Typography>
+                                                        </Box>
                                                     </Box>
+
                                                 </Box>
                                             )}
                                         </Draggable>
