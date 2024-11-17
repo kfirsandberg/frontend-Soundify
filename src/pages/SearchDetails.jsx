7c3d0b6262d27b749deb0b9966985b27ce78d760
amitbaruchh <amitbaruch08@gmail.com>
2024-11-17 12:28:02 +0200

    style for like songs button is search

--- src/pages/SearchDetails.jsx
+++ src/pages/SearchDetails.jsx
@@ -51,19 +51,13 @@ export function SearchDetails() {
             <h2>Songs</h2>
             <section className="song-list">
                 {searchedSongs
-                    .filter(song => song.duration)
-                    .map(song => (
-                        <div
-                            key={song.id}
-                            className="song-item"
-                        >
+                    .filter((song) => song.duration)
+                    .map((song) => (
+                        <div key={song.id} className="song-item">
                             {/* Song Image */}
-                            <div className="song-img" >
-                                <button >
-                                    <img
-                                        src={song.thumbnails[0]?.url}
-                                        alt={`${song.title} cover`}
-                                    />
+                            <div className="song-img">
+                                <button>
+                                    <img src={song.thumbnails[0]?.url} alt={`${song.title} cover`} />
                                     <div className="img-overlay">
                                         <div className="play-icon"></div>
                                     </div>
@@ -71,58 +65,46 @@ export function SearchDetails() {
                             </div>
 
                             {/* Song Details */}
-                            <div className="song-details" >
-                                <span
-                                    className="song-title"
-                                >
-                                    {song.title}
-                                </span>
-                                <span
-                                    className="song-artist"
-                                >
-                                    {song.artist}
-                                </span>
+                            <div className="song-details">
+                                <span className="song-title">{song.title}</span>
+                                <span className="song-artist">{song.artist}</span>
                             </div>
 
-                            {/* Like Button */}
-                            <button
-                                title={song.liked ? 'Remove from liked songs' : 'Add to liked songs'}
-                                className="liked-songs-btn"
-                                onClick={() => toggleLike(song)}
-                            >
-                                {song.liked ? (
-                                    <svg
-                                        xmlns="http://www.w3.org/2000/svg"
-                                        role="img"
-                                        aria-hidden="true"
-                                        viewBox="0 0 16 16"
-                                        className="liked-icon"
-                                    >
-                                        <path d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm11.748-1.97a.75.75 0 0 0-1.06-1.06l-4.47 4.47-1.405-1.406a.75.75 0 1 0-1.061 1.06l2.466 2.467 5.53-5.53z" />
-                                    </svg>
-                                ) : (
-                                    <svg
-                                        xmlns="http://www.w3.org/2000/svg"
-                                        role="img"
-                                        aria-hidden="true"
-                                        viewBox="0 0 24 24"
-                                        className="not-liked-icon"
-                                    >
-                                        <path d="M11.999 3a9 9 0 1 0 0 18 9 9 0 0 0 0-18zm-11 9c0-6.075 4.925-11 11-11s11 4.925 11 11-4.925 11-11 11-11-4.925-11-11z" />
-                                        <path d="M17.999 12a1 1 0 0 1-1 1h-4v4a1 1 0 1 1-2 0v-4h-4a1 1 0 1 1 0-2h4V7a1 1 0 1 1 2 0v4h4a1 1 0 0 1 1 1z" />
-                                    </svg>
-                                )}
-                            </button>
-
-                            {/* Song Duration */}
-                            <div
-                                className="song-duration"
-                            >
-                                {formatTime(song.duration)}
+                            {/* Like Button and Duration */}
+                            <div className="song-actions">
+                                <button
+                                    title={song.liked ? 'Remove from liked songs' : 'Add to liked songs'}
+                                    className="liked-songs-btn"
+                                    onClick={() => toggleLike(song)}
+                                >
+                                    {song.liked ? (
+                                        <svg
+                                            xmlns="http://www.w3.org/2000/svg"
+                                            role="img"
+                                            aria-hidden="true"
+                                            viewBox="0 0 16 16"
+                                            className="liked-icon"
+                                        >
+                                            <path d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm11.748-1.97a.75.75 0 0 0-1.06-1.06l-4.47 4.47-1.405-1.406a.75.75 0 1 0-1.061 1.06l2.466 2.467 5.53-5.53z" />
+                                        </svg>
+                                    ) : (
+                                        <svg
+                                            xmlns="http://www.w3.org/2000/svg"
+                                            role="img"
+                                            aria-hidden="true"
+                                            viewBox="0 0 24 24"
+                                            className="not-liked-icon"
+                                        >
+                                            <path d="M11.999 3a9 9 0 1 0 0 18 9 9 0 0 0 0-18zm-11 9c0-6.075 4.925-11 11-11s11 4.925 11 11-4.925 11-11 11-11-4.925-11-11z" />
+                                            <path d="M17.999 12a1 1 0 0 1-1 1h-4v4a1 1 0 1 1-2 0v-4h-4a1 1 0 1 1 0-2h4V7a1 1 0 1 1 2 0v4h4a1 1 0 0 1 1 1z" />
+                                        </svg>
+                                    )}
+                                </button>
+                                <div className="song-duration">{formatTime(song.duration)}</div>
                             </div>
                         </div>
                     ))}
             </section>
         </section>
-    )
-}
+    );
+ }
