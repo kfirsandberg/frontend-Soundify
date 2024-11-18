d27cb56356b3385b1e26aab8fb19fb0efd5a5181
avivmor97 <aviv.mor97@gmail.com>
2024-11-18 18:32:33 +0200

    design mq css

--- src/cmps/MoblieNavbar.jsx
+++ src/cmps/MoblieNavbar.jsx
@@ -6,13 +6,44 @@ export function MobileNavbar() {
   return (
     <nav className="mobile-navbar">
       <NavLink to="/library" className="nav-btn library-btn">
-        Library
+      <svg className="library-btn-icon"
+                            xmlns="http://www.w3.org/2000/svg"
+                            data-encore-id="icon"
+                            role="img"
+                            aria-hidden="true"
+                            viewBox="0 0 24 24"
+                            style={{ fill: "white" }}
+                            
+                        >
+                            <path d="M14.5 2.134a1 1 0 0 1 1 0l6 3.464a1 1 0 0 1 .5.866V21a1 1 0 0 1-1 1h-6a1 1 0 0 1-1-1V3a1 1 0 0 1 .5-.866zM16 4.732V20h4V7.041l-4-2.309zM3 22a1 1 0 0 1-1-1V3a1 1 0 0 1 2 0v18a1 1 0 0 1-1 1zm6 0a1 1 0 0 1-1-1V3a1 1 0 0 1 2 0v18a1 1 0 0 1-1 1z" />
+                        </svg>
       </NavLink>
       <NavLink to="/browse" className="nav-btn browse-btn">
-        Browse
+      <svg className="browse-btn-icon"
+                            xmlns="http://www.w3.org/2000/svg"
+                            data-encore-id="icon"
+                            role="img"
+                            aria-hidden="true"
+                            viewBox="0 0 24 24"
+                            style={{ fill: "white" }}
+                           
+                        >
+                            <path d="M10.533 1.27893C5.35215 1.27893 1.12598 5.41887 1.12598 10.5579C1.12598 15.697 5.35215 19.8369 10.533 19.8369C12.767 19.8369 14.8235 19.0671 16.4402 17.7794L20.7929 22.132C21.1834 22.5226 21.8166 22.5226 22.2071 22.132C22.5976 21.7415 22.5976 21.1083 22.2071 20.7178L17.8634 16.3741C19.1616 14.7849 19.94 12.7634 19.94 10.5579C19.94 5.41887 15.7138 1.27893 10.533 1.27893ZM3.12598 10.5579C3.12598 6.55226 6.42768 3.27893 10.533 3.27893C14.6383 3.27893 17.94 6.55226 17.94 10.5579C17.94 14.5636 14.6383 17.8369 10.533 17.8369C6.42768 17.8369 3.12598 14.5636 3.12598 10.5579Z" />
+                        </svg>
       </NavLink>
-      <NavLink to="/home" className="nav-btn home-btn">
-        Home
+      <NavLink to="/" className="nav-btn home-btn">
+      <svg className="home-btn-icon"
+                                xmlns="http://www.w3.org/2000/svg"
+                                data-encore-id="icon"
+                                role="img"
+                                aria-hidden="true"
+                                viewBox="0 0 24 24"
+                            >
+                                <path
+                                    d="M13.5 1.515a3 3 0 0 0-3 0L3 5.845a2 2 0 0 0-1 1.732V21a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1v-6h4v6a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V7.577a2 2 0 0 0-1-1.732l-7.5-4.33z"
+                                    fill="#ffffff"
+                                />
+                            </svg>
       </NavLink>
     </nav>
   )
